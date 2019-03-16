a7feec7c863a15f64151ac5be6fe75af981630f0
xxxx <sam@corcos.io>
2019-03-16 16:48:46 -0400

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Next Firebase Boilerplate
+
+This is a boilerplate app that I use.
+
+run `npm i` and `npm run dev` and you will see that the app works.
+
+To reproduce the relocator bug, try upgrading `@zeit/webpack-asset-relocator-loader` to 0.2.3.
+
+run `npm run dev` and you'll see that the app no longer works.
---
+++ pages/profile/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import {
+  Layout
+} from '@corcos/components'
+
+import {
+  Head,
+  Format,
+  Navbar
+} from '../../components'
+import Signup from '../../components/Signup'
+import Login from '../../components/Login'
+
+import Context from '../../lib/context'
+
+// determines what to render if the user is not logged in
+const SignUpOrLogIn = (self) => {
+  if (self.context.signUpOrLogIn === 'logIn') {
+    return <Login {...self.props} />
+  }
+  if (self.context.signUpOrLogIn === 'forgot') {
+    return <div>Forgot</div>
+  }
+  return <Signup {...self.props} />
+}
+
+// determines what to render depending on whether or not the user is logged in
+const LoginStatus = (self) => {
+  if (self.context.currentUser.uid) {
+    return <div>logged in</div>
+  }
+  return SignUpOrLogIn(self)
+}
+
+class Users extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  static getInitialProps = ({ query }) => {
+    return { query }
+  }
+
+  static contextType = Context
+
+  render () {
+    return (
+      <>
+        <Head />
+        <Format />
+        <Navbar />
+        {LoginStatus(this)}
+        <Layout>
+          User Profile page
+          {this.context.signUpOrLogIn}
+
+          <style jsx>{`
+          
+          `}</style>
+        </Layout>
+      </>
+    )
+  }
+}
+
+export default Users
